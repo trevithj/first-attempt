2d557f511c532b18e7f8078bd407bb822d15458c
john.trevithick <john.trevithick@reveal.co.nz>
2020-06-28 21:11:56 +1200

    Split out rendering code, add global object.

--- apps/balanced-line2/main.js
+++ apps/balanced-line2/main.js
@@ -1,20 +1,28 @@
 (function () {
 
-	const getEl = (sel) => document.querySelector(sel);
-	const getEls = (sel) => document.querySelectorAll(sel);
+	const { getEl, render } = BASE;
 	const random = max => Math.ceil(Math.random() * max);
 	let intervalId;
-	const INITIAL_CASH = 0;
-	const INITIAL_RM = 900;
-
+	const INITIAL_RM = 1200;
+	const MAX_TICKS = 400;
+	const MONEY = {
+		variable: 0,
+		fixed: 0,
+		revenue: 0
+	};
+	const STATS = {
+		meanCap: [0,0,0,0,0,0,0],
+		meanUse: [0,0,0,0,0,0,0]
+	};
 
 	const makeLine = id => {
 		const rm = INITIAL_RM;
-		const stores = [3,3,3,3,3,0];
+		const stores = [3,3,3,3,3,3,0];
 		const tick = 0;
-		const ops = [0,0,0,0,0,0];
-		const cash = INITIAL_CASH;
-		return { id, rm, stores, tick, ops, cash, msg:'Ready to run' };
+		const ops = [0,0,0,0,0,0,0];
+		const money = {...MONEY};
+		const stats = {...STATS};
+		return { id, rm, stores, tick, ops, money, stats };
 	};
 	const lines = [0,1,2,3].map(makeLine);
 
@@ -27,7 +35,20 @@
 		state => getSt(state).map(v => Math.min(random(3)+1, v)),
 		state => getSt(state).map(v => Math.min(random(5), v)),
 		state => getSt(state).map(v => Math.min(random(7)-1, v)),
-	]
+	];
+
+	const nextMoney = (money, ops) => {
+		const newMoney = {...money };
+		newMoney.variable += ops[0]; // $1 per unit
+		newMoney.revenue += 2* ops[ops.length-1]; //$2 per unit
+		newMoney.fixed += 2; //$2 per tick
+		return newMoney;
+	}
+
+	const nextStats = (stats, ops) => {
+		const newStats = { ...stats};
+		return newStats;
+	}
 
 	const nextState = (thisState, n) => {
 		const nextOps = nextOpsFns[n];
@@ -39,55 +60,17 @@
 			st[i+1] += d;
 		});
 
-		const revenue = 2* ops[ops.length-1];
-		const expenses = tick === 299 ? 800 : 0;
-		let cash = thisState.cash - ops[0] + revenue - expenses;
-		const msgs = [
-			`RM costs:$${ops[0]}`,
-			`Revenue:$${revenue}`,
-			`Fixed expenses:$${expenses}`,
-			`Cash on hand:$${cash}`
-		];
-		return { id, tick: tick + 1, rm:st[0], stores:st.slice(1), ops, cash, msg: msgs.join('\t') };
-	}
+		const money = nextMoney(thisState.money, ops);
+		const stats = nextStats(thisState.stats, ops);
+		return { id, tick: tick + 1, rm:st[0], stores:st.slice(1), ops, money, stats };
+	};
 
-	const lineDivs = getEls('.lineDiv');
-	const tickSpan = getEl('#ticks');
 	const btnStep = getEl('#btnStep');
 	const btnRunS = getEl('#btnRunS');
 	const btnRunF = getEl('#btnRunF');
 	const btnStop = getEl('#btnStop');
 
 	const ticks = [lines];
-
-	const render = (data) => {
-		tickSpan.innerHTML = data[0].tick;
-		lineDivs.forEach((div, index) => {
-			// const failrate = JSON.parse(div.dataset.failrate);
-			// const runtimes = JSON.parse(div.dataset.runtimes);
-			// const stores = JSON.parse(div.dataset.stores);
-			// console.log({ index, failrate, runtimes, stores });
-			const { rm, stores, ops, cash, msg } = data[index];
-			const html = [
-				'<div>',
-				`<button class="raw-material">${rm}</button>`,
-				...stores.map((s, i) => {
-					return [
-						'<span>',
-						'	<span>&#8680;</span>',
-						`	<span class="op">${ops[i]}</span>`,
-						'	<span>&#8680;</span>',
-						`	<button class="store">${s}</button>`,
-						'</span>'
-					].join("");
-				}),
-				`<span class="cash">$${cash}</cash>`,
-				`<pre>${msg}</pre>`,
-				'</div>',
-			];
-			div.innerHTML = html.join('');
-		});
-	};
 
 	const doTick = () => {
 		const thisLines = ticks[ticks.length-1];
@@ -102,7 +85,7 @@
 	const doRun = () => {
 		doTick();
 		render(ticks[ticks.length-1]);
-		if(ticks.length > 300) {
+		if(ticks.length > MAX_TICKS) {
 			doStop();
 		}
 	};
@@ -112,7 +95,7 @@
 	}
 	btnStep.addEventListener('click', () => { doStop(); doRun(); });
 	btnRunS.addEventListener('click', () => doRuns(500));
-	btnRunF.addEventListener('click', () => doRuns(100));
+	btnRunF.addEventListener('click', () => doRuns(50));
 	btnStop.addEventListener('click', doStop);
 	document.addEventListener('beforeunload', doStop);
 
---
+++ apps/balanced-line2/render.js
@@ -0,0 +1,51 @@
+/* global BASE */
+(function () {
+
+	const { getEl, getEls } = BASE;
+
+	const lineDivs = getEls('.lineDiv');
+	const tickSpan = getEl('#ticks');
+
+	const renderMoney = money => {
+		const { revenue, fixed, variable } = money;
+		const np = revenue - fixed - variable;
+		const msgs = [
+			`Revenue:$${revenue}`,
+			`Variable costs:$-${variable}`,
+			`Fixed costs:$-${fixed}`,
+			`Net Profit:$${np}`
+		];
+		return { np, msg: msgs.join('\t') };
+	}
+
+	BASE.render = (data) => {
+		tickSpan.innerHTML = data[0].tick;
+		lineDivs.forEach((div, index) => {
+			// const failrate = JSON.parse(div.dataset.failrate);
+			// const runtimes = JSON.parse(div.dataset.runtimes);
+			// const stores = JSON.parse(div.dataset.stores);
+			// console.log({ index, failrate, runtimes, stores });
+			const { rm, stores, ops, money } = data[index];
+			const { np, msg } = renderMoney(money);
+			const html = [
+				'<div>',
+				`<div class="box raw-material">${rm}</div>`,
+				...stores.map((s, i) => {
+					return [
+						'<span>',
+						'	<span>&#8680;</span>',
+						`	<div class="box op">${ops[i]}</div>`,
+						'	<span>&#8680;</span>',
+						`	<div class="box store">${s}</div>`,
+						'</span>'
+					].join("");
+				}),
+				`<span class="cash">$${np}</cash>`,
+				`<pre>${msg}</pre>`,
+				'</div>',
+			];
+			div.innerHTML = html.join('');
+		});
+	};
+
+})();
